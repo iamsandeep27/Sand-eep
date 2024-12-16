4b5dc7e9a52dc964e95cc93f28dd48aedbd02287
iamsandeep27 <i.am.ad.sandeep@gmail.com>
2024-12-16 10:31:29 +0545

    First Commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// for skill , education and experience mouse point (click works)-->
+/*   
+var tablinks =document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}*/
